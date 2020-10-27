fb61fb4f540b48478cd1a52a225ce95d920dcb78
rengzhengcodes <rzhengcontacts@gmail.com>
2020-10-27 14:33:20 -0400

    BankAccount.java with required methods for class added so that they can be called. Has not been tested.

---
+++ BankAccount.java
@@ -0,0 +1,46 @@
+public class BankAccount {
+  private double balance;
+  private int accountID;
+  private String password;
+
+  public BankAccount(int ID, String pw) {
+    balance = 0;
+    accountID = ID;
+    password = pw;
+  }
+
+  public double getBalance() {
+    return balance;
+  }
+
+  public int getAccountID() {
+    return accountID;
+  }
+
+  public void setPassword(String newPass) {
+    password = newPass;
+  }
+
+  public boolean deposit(double amount) {
+    if (amount < 0) {
+      return false;
+    } else {
+      balance += amount;
+      return true;
+    }
+  }
+
+  public boolean withdraw(double amount) {
+    if (amount < 0 || amount > balance) {
+      return false;
+    } else {
+      balance -= amount;
+      return true;
+    }
+  }
+
+  public String toString() {
+    return accountID + "\\t" + balance;
+  }
+  
+}
