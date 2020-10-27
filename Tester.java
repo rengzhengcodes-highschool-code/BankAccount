76941ea020a6044e7fa87ef4ebf77f30ee9f1f1c
rengzhengcodes <rzhengcontacts@gmail.com>
2020-10-27 15:48:58 -0400

    Tester function updated to test the getBalance and deposit functions respectively. All tests passed.

--- Tester.java
+++ Tester.java
@@ -1,6 +1,27 @@
 public class Tester {
 
   public static void main(String[] args) {
+    idTester();
+    balanceTester();
+  }
+
+
+
+  public static void errorHorn() {
+    System.out.println("ERROR!\nERROR\nERROR");
+  }
+
+  public static void idEqualityTester(BankAccount bankAccount, int ID) {
+    if (ID != bankAccount.getAccountID()) {
+      errorHorn();
+      System.out.println("Constructor failed at assigning bank ID or getAccountID method failed: " + ID +
+      "\n Returned: " + bankAccount.getAccountID());
+    } else {
+      System.out.println("ID assignment and retrieval works.");
+    }
+  }
+
+  public static void idTester() {
     int[] bankIDs = {10234, 032432, 0, -423432, 204321, 232, 2, -2321, -2};
     System.out.println("POSITIVE ID RANDOMIZED TEST");
     for (int i = 0; i < 10; i++) {
@@ -21,21 +42,39 @@ public class Tester {
       BankAccount account = new BankAccount(bankIDs[index], "");
       idEqualityTester(account, bankIDs[index]);
     }
-
   }
 
-  public static void errorHorn() {
-    System.out.println("ERROR!\nERROR\nERROR");
-  }
+  public static void balanceTester() {
+    int[] depositAmounts = {0, 121321321, -21432, -2, 1};
+    boolean[] depositTestResults = {true, true, false, false, true};
+    BankAccount[] bankAccounts = new BankAccount[5];
 
-  public static void idEqualityTester(BankAccount bankAccount, int ID) {
-    if (ID != bankAccount.getAccountID()) {
-      errorHorn();
-      System.out.println("Constructor failed at assigning bank ID or getAccountID method failed: " + ID +
-      "\n Returned: " + bankAccount.getAccountID());
-    } else {
-      System.out.println("ID assignment and retrieval works.");
+    for (int index = 0; index < bankAccounts.length; index++) {
+      BankAccount account = new BankAccount(1, "");
+      bankAccounts[index] = account;
     }
+
+    System.out.println("deposit() TEST");
+    for (int index = 0; index < depositAmounts.length; index++) {
+      if (bankAccounts[index].deposit(depositAmounts[index]) == depositTestResults[index]) {
+        System.out.println("deposit function succeeded.");
+      } else {
+        errorHorn();
+        System.out.println("deposit function failed");
+      }
+    }
+
+    System.out.println("getBalance() TEST");
+    int[] expectedBal = {0, 121321321, 0, 0, 1};
+    for (int index = 0; index < expectedBal.length; index++) {
+      if (bankAccounts[index].getBalance() == expectedBal[index]) {
+        System.out.println("getBalance + deposit function succeeded.");
+      } else {
+        errorHorn();
+        System.out.println("Depoist or getBalance function failed");
+      }
+    }
+
   }
 
 }
